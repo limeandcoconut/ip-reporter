c6d89c10cc6b535a944c79f7c813e66589150758
Jacob Smith <js@jacobsmith.tech>
2020-03-22 22:07:38 -0700

    Fix prod endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const isProd = require('./utils').isProd()
 
 module.exports = {
-  recorderHost: isProd ? 'http://54.200.31.197:3257' : 'http://localhost:3257',
+  recorderHost: isProd ? 'http://ip-recorder.jacobsmith.tech' : 'http://localhost:3257',
 }
